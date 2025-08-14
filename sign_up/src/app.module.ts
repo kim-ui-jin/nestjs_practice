f355b4cb4115b54a948874ae6c445f830ab83126
Kim-ui-jin <yd5663@gmail.com>
2025-08-14 15:32:23 +0900

    board 만들기

--- sign_up/src/app.module.ts
+++ sign_up/src/app.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { BoardModule } from './board/board.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, BoardModule],
   controllers: [AppController],
   providers: [AppService],
 })
